daa3a147fb1545260670950c78aeb493b691f025
Marco Aceti <mail@marcoaceti.it>
2020-12-01 18:45:59 +0100

    Day 1

---
+++ 2020/day-01/main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+func main() {
+	b := bufio.NewScanner(os.Stdin)
+	var input []int
+	for b.Scan() {
+		n, err := strconv.Atoi(b.Text())
+		if err != nil {
+			fmt.Println("Error!")
+		}
+		input = append(input, n)
+	}
+
+Part1Loop:
+	for _, n := range input {
+		for _, k := range input {
+			if n+k == 2020 {
+				fmt.Printf("Answer (part 1): %d\n", n*k)
+				break Part1Loop
+			}
+		}
+	}
+
+Part2Loop:
+	for _, n := range input {
+		for _, k := range input {
+			for _, j := range input {
+				if n+k+j == 2020 {
+					fmt.Printf("Answer (part 2): %d\n", n*k*j)
+					break Part2Loop
+				}
+			}
+		}
+	}
+}
---
+++ README.md
@@ -0,0 +1,4 @@
+# Advent of Code 2020
+
+My solutions for [Advent of Code](https://adventofcode.com) challenges, 
+until I'll forget about it.
