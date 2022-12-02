5d0fe50d4e4adc2b8669f0593ada1548ca9caf55
Marco Aceti <mail@marcoaceti.it>
2022-12-02 18:54:41 +0100

    [2022/02] Refactor remove useless inner mod

--- 2022/day-02/src/tests.rs
+++ 2022/day-02/src/tests.rs
@@ -1,85 +1,85 @@
 #[cfg(test)]
-mod tests {
-    use crate::{Game, GameResult, Outcome, Shape};
 
-    #[test]
-    fn test_part1_ay() {
-        let game = Game {
-            opponent: Shape::Rock,
-            myself: Shape::Paper,
-        };
-        assert_eq!(
-            game.play(),
-            GameResult {
-                outcome: Outcome::Win,
-                my_points: 8
-            }
-        );
-    }
+use crate::{Game, GameResult, Outcome, Shape};
 
-    #[test]
-    fn test_part1_bx() {
-        let game = Game {
-            opponent: Shape::Paper,
-            myself: Shape::Rock,
-        };
-        assert_eq!(
-            game.play(),
-            GameResult {
-                outcome: Outcome::Lose,
-                my_points: 1
-            }
-        );
-    }
-
-    #[test]
-    fn test_part1_cz() {
-        let game = Game {
-            opponent: Shape::Scissors,
-            myself: Shape::Scissors,
-        };
-        assert_eq!(
-            game.play(),
-            GameResult {
-                outcome: Outcome::Draw,
-                my_points: 6
-            }
-        );
-    }
-
-    #[test]
-    fn test_part2_ay() {
-        let game = Game::from_prediction(Shape::Rock, Outcome::Draw);
-        assert_eq!(
-            game.play(),
-            GameResult {
-                outcome: Outcome::Draw,
-                my_points: 4
-            }
-        );
-    }
-
-    #[test]
-    fn test_part2_bx() {
-        let game = Game::from_prediction(Shape::Paper, Outcome::Lose);
-        assert_eq!(
-            game.play(),
-            GameResult {
-                outcome: Outcome::Lose,
-                my_points: 1
-            }
-        );
-    }
-
-    #[test]
-    fn test_part2_cz() {
-        let game = Game::from_prediction(Shape::Scissors, Outcome::Win);
-        assert_eq!(
-            game.play(),
-            GameResult {
-                outcome: Outcome::Win,
-                my_points: 7
-            }
-        );
-    }
+#[test]
+fn test_part1_ay() {
+    let game = Game {
+        opponent: Shape::Rock,
+        myself: Shape::Paper,
+    };
+    assert_eq!(
+        game.play(),
+        GameResult {
+            outcome: Outcome::Win,
+            my_points: 8
+        }
+    );
 }
+
+#[test]
+fn test_part1_bx() {
+    let game = Game {
+        opponent: Shape::Paper,
+        myself: Shape::Rock,
+    };
+    assert_eq!(
+        game.play(),
+        GameResult {
+            outcome: Outcome::Lose,
+            my_points: 1
+        }
+    );
+}
+
+#[test]
+fn test_part1_cz() {
+    let game = Game {
+        opponent: Shape::Scissors,
+        myself: Shape::Scissors,
+    };
+    assert_eq!(
+        game.play(),
+        GameResult {
+            outcome: Outcome::Draw,
+            my_points: 6
+        }
+    );
+}
+
+#[test]
+fn test_part2_ay() {
+    let game = Game::from_prediction(Shape::Rock, Outcome::Draw);
+    assert_eq!(
+        game.play(),
+        GameResult {
+            outcome: Outcome::Draw,
+            my_points: 4
+        }
+    );
+}
+
+#[test]
+fn test_part2_bx() {
+    let game = Game::from_prediction(Shape::Paper, Outcome::Lose);
+    assert_eq!(
+        game.play(),
+        GameResult {
+            outcome: Outcome::Lose,
+            my_points: 1
+        }
+    );
+}
+
+#[test]
+fn test_part2_cz() {
+    let game = Game::from_prediction(Shape::Scissors, Outcome::Win);
+    assert_eq!(
+        game.play(),
+        GameResult {
+            outcome: Outcome::Win,
+            my_points: 7
+        }
+    );
+}
+
