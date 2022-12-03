1953e2f57c2093f2d16add9f003f219bc28c39c8
Marco Aceti <mail@marcoaceti.it>
2022-12-03 15:04:22 +0100

    [2022/03] Add part 2

--- 2022/day-03/src/main.rs
+++ 2022/day-03/src/main.rs
@@ -1,7 +1,9 @@
 use std::fs;
+
+#[cfg(test)]
 mod tests;
 
-#[derive(Debug, PartialEq, Eq)]
+#[derive(Debug, PartialEq, Eq, Clone)]
 struct Item(char);
 
 impl Item {
@@ -21,7 +23,7 @@ impl Item {
 
 type Compartment = Vec<Item>;
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 struct Rucksack {
     compartment_1: Compartment,
     compartment_2: Compartment,
@@ -41,21 +43,58 @@ impl Rucksack {
             .iter()
             .find(|c| self.compartment_2.contains(c))
     }
+
+    fn contents(&self) -> Vec<Item> {
+        self.compartment_1
+            .iter()
+            .cloned()
+            .chain(self.compartment_2.iter().cloned())
+            .collect()
+    }
+}
+
+#[derive(Debug)]
+struct Group(Rucksack, Rucksack, Rucksack);
+
+impl Group {
+    fn new(items_1: &str, items_2: &str, items_3: &str) -> Self {
+        Group(
+            Rucksack::new(items_1),
+            Rucksack::new(items_2),
+            Rucksack::new(items_3),
+        )
+    }
+
+    fn common_item(&self) -> Option<Item> {
+        self.0
+            .contents()
+            .iter()
+            .find(|c| self.1.contents().contains(c) && self.2.contents().contains(c))
+            .cloned()
+    }
 }
 
 fn main() {
     let contents = fs::read_to_string("input.txt").unwrap();
 
-    let mut sum = 0;
-    for line in contents.lines() {
-        if line.is_empty() {
-            break;
-        }
+    let mut lines = contents.lines();
+    let mut part_1 = 0;
+    let mut part_2 = 0;
+    while let (Some(line_1), Some(line_2), Some(line_3)) =
+        (lines.next(), lines.next(), lines.next())
+    {
+        let group = Group::new(line_1, line_2, line_3);
 
-        let rucksack = Rucksack::new(line);
-        sum += rucksack.common_item().unwrap().priority();
+        part_1 += group.0.common_item().unwrap().priority();
+        part_1 += group.1.common_item().unwrap().priority();
+        part_1 += group.2.common_item().unwrap().priority();
+
+        part_2 += group.common_item().unwrap().priority();
     }
 
-    println!("Part 1: {}", sum);
-    assert_eq!(sum, 7824);
+    println!("Part 1: {}", part_1);
+    assert_eq!(part_1, 7824);
+
+    println!("Part 2: {}", part_2);
+    assert_eq!(part_2, 2798);
 }
--- 2022/day-03/src/tests.rs
+++ 2022/day-03/src/tests.rs
@@ -1,6 +1,15 @@
+const LINES: [&'static str; 6] = [
+    "vJrwpWtwJgWrhcsFMMfFFhFp",
+    "jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL",
+    "PmmdzqPrVvPwwTWBwg",
+    "wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn",
+    "ttgJtRGJQctTZtZT",
+    "CrZsJsPPZsGzwwsLwLmpwMDw",
+];
+
 #[cfg(test)]
 mod part1 {
-    use crate::Rucksack;
+    use crate::{tests::LINES, Rucksack};
 
     fn get_priority(input: &str) -> Option<u32> {
         let rucksack = Rucksack::new(input);
@@ -12,31 +21,54 @@ mod part1 {
 
     #[test]
     fn input_1() {
-        assert_eq!(get_priority("vJrwpWtwJgWrhcsFMMfFFhFp"), Some(16));
+        assert_eq!(get_priority(LINES[0]), Some(16));
     }
 
     #[test]
     fn input_2() {
-        assert_eq!(get_priority("jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL"), Some(38));
+        assert_eq!(get_priority(LINES[1]), Some(38));
     }
 
     #[test]
     fn input_3() {
-        assert_eq!(get_priority("PmmdzqPrVvPwwTWBwg"), Some(42));
+        assert_eq!(get_priority(LINES[2]), Some(42));
     }
 
     #[test]
     fn input_4() {
-        assert_eq!(get_priority("wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn"), Some(22));
+        assert_eq!(get_priority(LINES[3]), Some(22));
     }
 
     #[test]
     fn input_5() {
-        assert_eq!(get_priority("ttgJtRGJQctTZtZT"), Some(20));
+        assert_eq!(get_priority(LINES[4]), Some(20));
     }
 
     #[test]
     fn input_6() {
-        assert_eq!(get_priority("CrZsJsPPZsGzwwsLwLmpwMDw"), Some(19));
+        assert_eq!(get_priority(LINES[5]), Some(19));
+    }
+}
+
+#[cfg(test)]
+mod part2 {
+    use crate::{tests::*, Group};
+
+    fn get_priority(line_1: &str, line_2: &str, line_3: &str) -> Option<u32> {
+        let group = Group::new(line_1, line_2, line_3);
+        match group.common_item() {
+            Some(item) => Some(item.priority()),
+            None => None,
+        }
+    }
+
+    #[test]
+    fn input_1() {
+        assert_eq!(get_priority(LINES[0], LINES[1], LINES[2]), Some(18));
+    }
+
+    #[test]
+    fn input_2() {
+        assert_eq!(get_priority(LINES[3], LINES[4], LINES[5]), Some(52));
     }
 }
